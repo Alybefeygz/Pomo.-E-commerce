7175bf46505a9a875a658268dd89b8ddad0076e2
Yağız Efe ALAYBAY <ygzefealyb@gmail.com>
2025-04-26 15:10:08 +0300

    Collective-Cals-Backend-Commit

--- hesaplama/api/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import User
-from django.core.validators import MinValueValidator, MaxValueValidator
-from decimal import Decimal
-
-# Create your models here. 
--- profiller/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from profiller.models import Profil, ProfilDurum
-# Register your models here.
-
-
-admin.site.register(Profil)
-admin.site.register(ProfilDurum)
